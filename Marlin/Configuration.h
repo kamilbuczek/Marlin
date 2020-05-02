8429ead0c148d99a3ff8c413b80131d6d69c9ab8
Kamil Buczek <buczek.kamil@gmail.com>
2020-05-02 16:52:59 +0200

    PID Tuning + SD Card ONBoard

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 22.63
+  #define DEFAULT_Ki 1.79
+  #define DEFAULT_Kd 71.39
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1235,7 +1235,7 @@
    *
    * :[ 'LCD', 'ONBOARD', 'CUSTOM_CABLE' ]
    */
-  //#define SDCARD_CONNECTION LCD
+#define SDCARD_CONNECTION ONBOARD
 
 #endif // SDSUPPORT
 
